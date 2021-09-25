1a72a21149c896e67fd78656eac32d3718b96ee4
Bryn McIvor <brynmcivor1@gmail.com>
2021-09-26 00:40:30 +1200

    add project overview to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,7 +6,22 @@ export const Homepage = () => {
     <SiteBackground>
       <div className="homepage">
         <h1>Homepage</h1>
-        <p>This is the homepage</p>
+
+        <p>Created this website to learn more about web design.</p>
+
+        <p>
+          <a href="https://github.com/brynious/pillarboxd-frontend" target="_blank">
+            Front-end
+          </a>{' '}
+          made using React, context-api, react-router, and Tailwind CSS.
+        </p>
+
+        <p>
+          <a href="https://github.com/brynious/pillarboxd-backend" target="_blank">
+            Back-end
+          </a>{' '}
+          made using Node, Express, JSON Web Token, and Bcrypt.
+        </p>
       </div>
     </SiteBackground>
   );
