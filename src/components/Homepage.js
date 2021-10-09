33dc4c8b9b7dc8635b5192be52eb5de4f147b696
Bryn McIvor <brynmcivor1@gmail.com>
2021-10-09 14:41:42 +1300

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,20 @@
 // import React from 'react';
-import { SiteBackground } from './SiteBackground';
+import { Link } from 'react-router-dom';
 
 export const Homepage = () => {
   return (
     <div className="homepage">
       <div class="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-        <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+        <div class="flex flex-col w-full xl:w-1/2 justify-center lg:items-start overflow-y-hidden">
           <h1 class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+            Welcome to{' '}
             <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">Pillarboxd</span>
           </h1>
           <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
-            Track the TV shows you're watching this lockdown and beyond!
+            <Link to="/signup" className="underline">
+              Sign Up
+            </Link>{' '}
+            and start tracking the TV shows you're watching
           </p>
         </div>
       </div>
